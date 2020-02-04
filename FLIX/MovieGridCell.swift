a93627d96f482b81da0bafe874710442e5f493b6
Tu Anh Nguyen <nguat2018@gmail.com>
2020-02-04 15:10:42 -0800

    Finish Collection View Setup

---
+++ FLIX/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  FLIX
+//
+//  Created by Tu A Nguyen on 2/4/20.
+//  Copyright © 2020 Tu Anh Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
